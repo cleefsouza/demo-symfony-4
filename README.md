08bc015c13cb47041cea85031146b40f3c3723c0
cleefsouza <a.cleef.souza@gmail.com>
2020-02-04 22:50:22 -0300

    Commit inicial

---
+++ README.md
@@ -0,0 +1,13 @@
+## SYMFONY 4
+Projeto introdutorio ao framework PHP Symfony 4.
+
+---
+
+#### COMANDOS
+
+* Cria um projeto `composer create-project symfony/skeleton app-name`
+* Roda o projeto localmente `php - S localhost:8000 -t public`
+* Instala servidor do Symfony `composer require symfony/web-server-bundle ^4.4.2`
+* Executa servidor do Symfony `php bin/console server:run`
+* Instala annotations `composer require annotations`
+* Instala o Twig para criação de templates `composer require twig`
---
+++ src/Controller/ExemploController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ExemploController extends AbstractController {
+
+    /**
+     * @return Response
+     * @Route("page-exemplo")
+     */
+    public function index() {
+        return new Response(
+            "<html><body><h3>Exemplo de Controller</h3></body></html>"
+        );
+    }
+
+    /**
+     * @return Response
+     * @Route("page-twig")
+     */
+    public function pageTwig() {
+        return $this->render("exemplo/exemplo.html.twig", [
+            "alerta" => "Eu sou um alerta!"
+        ]);
+    }
+}
